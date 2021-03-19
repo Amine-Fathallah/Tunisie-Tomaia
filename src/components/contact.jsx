ac6c782646c96dff5f43b21ae171776c8e3c7358
Amine-Fathallah <a.fathallah.2021@gmail.com>
2021-03-19 11:10:10 +0100

    Tunisie Tomaia

---
+++ src/components/contact.jsx
@@ -0,0 +1,216 @@
+import React, { Component } from "react";
+
+export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: "",
+    };
+  }
+  render() {
+    const { status } = this.state;
+    return (
+      <div>
+        <div id="contact">
+          <div className="container">
+            <div className="col-md-8">
+              <div className="row">
+                <div className="section-title">
+                  <h2>Entrer en contact</h2>
+                  <p>
+                    Veuillez remplir le formulaire ci-dessous pour nous envoyer
+                    un e-mail et nous vous répondrons dans les plus brefs
+                    délais.
+                  </p>
+                </div>
+                <form
+                  name="sentMessage"
+                  id="contactForm"
+                  onSubmit={this.submitForm}
+                  action="https://formspree.io/f/xzbkwylj"
+                  method="POST"
+                  noValidate
+                >
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          id="firstName"
+                          name="firstName"
+                          className="form-control"
+                          placeholder="Nom"
+                          required="required"
+                        />
+                        <p className="help-block text-danger"></p>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          id="lastName"
+                          name="lastName"
+                          className="form-control"
+                          placeholder="Prénom"
+                          required="required"
+                        />
+                        <p className="help-block text-danger"></p>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          className="form-control"
+                          placeholder="Email"
+                          required="required"
+                        />
+                        <p className="help-block text-danger"></p>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          id="phoneNumber"
+                          name="phoneNumber"
+                          className="form-control"
+                          placeholder="Téléphone"
+                          required="required"
+                        />
+                        <p className="help-block text-danger"></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      name="message"
+                      id="message"
+                      name="message"
+                      className="form-control"
+                      rows="4"
+                      placeholder="Message"
+                      required
+                    ></textarea>
+                    <p className="help-block text-danger"></p>
+                  </div>
+
+                  <div id="success">
+                    {status === "SUCCESS" ? (
+                      <div>
+                        <button
+                          onclick="myFunction()"
+                          type="submit"
+                          className="btn btn-custom btn-lg"
+                        >
+                          Envoyer
+                        </button>
+                        <script>
+                          function myFunction(){" "}
+                          {alert("Your mail is sent, Thanks!")}
+                        </script>
+                      </div>
+                    ) : (
+                      <button type="submit" className="btn btn-custom btn-lg">
+                        Envoyer
+                      </button>
+                    )}
+                    {status === "ERROR" && (
+                      <script>
+                        function myFunction(){" "}
+                        {alert("Ooops! There was an error!")}
+                      </script>
+                    )}
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="col-md-3 col-md-offset-1 contact-info">
+              <div className="contact-item">
+                <h3>Contact Info</h3>
+                <p>
+                  <span>
+                    <i className="fa fa-map-marker"></i> Address
+                  </span>
+                  {this.props.data ? this.props.data.address : "loading"}
+                </p>
+              </div>
+              <div className="contact-item">
+                <p>
+                  <span>
+                    <i className="fa fa-phone"></i> Phone
+                  </span>{" "}
+                  {this.props.data ? this.props.data.phone : "loading"}
+                </p>
+              </div>
+              <div className="contact-item">
+                <p>
+                  <span>
+                    <i className="fa fa-envelope-o"></i> Email
+                  </span>{" "}
+                  {this.props.data ? this.props.data.email : "loading"}
+                </p>
+              </div>
+            </div>
+            <div className="col-md-12">
+              <div className="row">
+                <div className="social">
+                  <ul>
+                    <li>
+                      <a
+                        href={this.props.data ? this.props.data.facebook : "/"}
+                      >
+                        <i className="fa fa-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={this.props.data ? this.props.data.twitter : "/"}>
+                        <i className="fa fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={this.props.data ? this.props.data.youtube : "/"}>
+                        <i className="fa fa-youtube"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="footer">
+          <div className="container text-center">
+            <p>
+              &copy; {new Date().getFullYear()} Copyrights:{" "}
+              <a href="/"> Tunisie Tomaia </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
---
+++ src/components/gallery.jsx
@@ -0,0 +1,215 @@
+import React, { Component } from "react";
+
+export class Gallery extends Component {
+  render() {
+    return (
+      <div id="portfolio" className="text-center">
+        <div className="container">
+          <div className="section-title">
+            <h2>Galerie</h2>
+            <p>
+              « Les meilleures choses qui arrivent dans le monde de l’entreprise
+              ne sont pas le résultat du travail d’un seul homme. C’est le
+              travail de toute une équipe » – Steve Jobs
+            </p>
+          </div>
+          <div className="row">
+            <div className="portfolio-items">
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/01-large.jpg"
+                      title="Coupeur(se)"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Coupeur(se)</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/01-small.jpg"
+                        className="img-responsive"
+                        alt="Coupeur(se)"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/02-large.jpg"
+                      title="Piqueur(se) apprêteur(se)"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Piqueur(se) apprêteur(se)</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/02-small.jpg"
+                        className="img-responsive"
+                        alt="Piqueur(se) apprêteur(se)"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/03-large.jpg"
+                      title="Coupeur(se)"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Coupeur(se)</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/03-small.jpg"
+                        className="img-responsive"
+                        alt="Coupeur(se)"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/04-large.jpg"
+                      title="Piqueur(se) apprêteur(se)"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Piqueur(se) apprêteur(se)</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/04-small.jpg"
+                        className="img-responsive"
+                        alt="Piqueur(se) apprêteur(se)"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/05-large.jpg"
+                      title="Contrôleur(se)"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Contrôleur(se)</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/05-small.jpg"
+                        className="img-responsive"
+                        alt="Contrôleur(se)"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/06-large.jpg"
+                      title="Piqueur(se) apprêteur(se)"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Piqueur(se) apprêteur(se)</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/06-small.jpg"
+                        className="img-responsive"
+                        alt="Piqueur(se) apprêteur(se)"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/07-large.jpg"
+                      title="Piqueur(se) apprêteur(se)"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Piqueur(se) apprêteur(se)</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/07-small.jpg"
+                        className="img-responsive"
+                        alt="Piqueur(se) apprêteur(se)"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/08-large.jpg"
+                      title="Magasinier(e)"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Magasinier(e)</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/08-small.jpg"
+                        className="img-responsive"
+                        alt="Magasinier(e)"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a
+                      href="img/portfolio/09-large.jpg"
+                      title="Contrôleur(se)"
+                      data-lightbox-gallery="gallery1"
+                    >
+                      <div className="hover-text">
+                        <h4>Contrôleur(se)</h4>
+                      </div>
+                      <img
+                        src="img/portfolio/09-small.jpg"
+                        className="img-responsive"
+                        alt="Contrôleur(se)"
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Gallery;
